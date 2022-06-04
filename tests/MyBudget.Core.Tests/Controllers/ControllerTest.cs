d4959f0844226330daf6f5f778d1ce128fe3c075
Felipe Alves <felipe.alves@itlab.com.br>
2022-06-03 22:51:59 -0300

    Add controler test

---
+++ tests/MyBudget.Core.Tests/Controllers/ControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Core.Tests.Controllers
+{
+    public abstract class ControllerTest
+    {
+        protected string UserId { get; set; }
+
+        public ControllerTest()
+        {
+            UserId = Guid.NewGuid().ToString();
+        }
+
+        protected void AddControllerContext(ControllerBase controller)
+        {
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.SetupGet(x => x.User).Returns(GetIdentity());
+
+            controller.ControllerContext = new ControllerContext() {
+                HttpContext = mockHttpContext.Object
+            };
+        }
+
+        private ClaimsPrincipal GetIdentity()
+        {
+            List<Claim> claims = new();
+            claims.Add(new Claim("Id", UserId));
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims);
+            return new ClaimsPrincipal(identity);
+        }
+
+        protected object GetValueInResult(ActionResult result)
+        {
+            object value = new object { };
+
+            PropertyInfo[] properties = result.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                if(property.Name == "Value")
+                {
+                    value = property.GetValue(result) ?? new object { };
+                }
+            }
+
+            return value;
+        }
+    }
+}
